0dc30658be1308eea13144b903cf5d136b0cfe91
Maruf Khan <marufatrai12@gmail.com>
2023-11-20 09:16:09 +0600

    JS-Class-13

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+
+
+    
+    
+    //  Javascript if else Conditional Statements.
+
+
+    var num = prompt("Enter a Number : ");
+
+    var x = 0 ;
+
+    if(num > x){
+
+        document.write("Positive");
+
+    }else if(num < x){
+
+        document.write("Negitive");
+
+    }else{
+
+        document.write("Zero");
+
+    }
